6f402e91aa792ff9e208df022dc9a2545076a12a
dotevo <dotevo@gmail.com>
2015-02-11 21:46:43 +0100

    Refactoring

---
+++ js/layers.js
@@ -0,0 +1,258 @@
+Object.size = function(obj) {
+	var size = 0, key;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) size++;
+	}
+	return size;
+};
+
+L.OwnLayersPack = L.Class.extend({
+	options: {
+		wayColors: {noneway:"yellow",opposite_lane:"orange",lane: "green", shared:"purple",cycleway:"red"}
+	},
+
+    initialize: function (options) {
+		var _this = this;
+        L.Util.setOptions(this, options);
+		this._overpassQueue = new L.OverpassQueue({
+			//onLoadingStarted: function(){$("#text-zbox-loading").css("display","inline");},
+			//onLoadingFinished: function(){$("#text-zbox-loading").css("display","none");}
+		});
+		//POI
+		this._poiLayer = L.markerClusterGroup({ 
+					disableClusteringAtZoom: 14,
+					iconCreateFunction: function(cluster) {
+						var childCount = cluster.getChildCount();
+						return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>',
+								className: 'marker-cluster marker-cluster-poi',
+								iconSize: new L.Point(40, 40) });
+					}
+				});
+		this._addPOIPart();
+		//Hazard
+		
+		this._hazardLayer = L.markerClusterGroup({
+					disableClusteringAtZoom: 14,
+					iconCreateFunction: function(cluster) {
+						var childCount = cluster.getChildCount();
+						return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>',
+								className: 'marker-cluster marker-cluster-hazard',
+								iconSize: new L.Point(40, 40) });
+					}
+					});
+		this._addHazardPart();
+
+		//Way
+		this._wayLayer = L.featureGroup();
+		this._addWayPart();
+	/*	this._waysZoomedLayer = L.featureGroup();
+		this.options.map.on("zoomend",function(){
+				if(_this.options.map.getZoom() > 17){
+					_this.options.map.addLayer(_this._waysZoomedLayer);
+				}else{
+					_this.options.map.removeLayer(_this._waysZoomedLayer);
+				}
+			});*/
+		//Trail
+		this._trailLayer = L.featureGroup();
+		this._addTrailPart();
+	},
+
+	_hideLayer: function(layer,layer2){
+		if(this.options.map.hasLayer(layer))
+			this.options.map.removeLayer(layer);
+	},
+
+	_showLayer: function(layer){
+		if(!this.options.map.hasLayer(layer))
+			this.options.map.addLayer(layer);
+	},
+
+	hidePOILayer: function(){
+		this._hideLayer(this._poiLayer);
+		this._hideLayer(this._poifetcher);
+	},
+
+	showPOILayer: function(){
+		this._showLayer(this._poiLayer);
+		this._showLayer(this._poifetcher);
+	},
+
+	_addPOIPart: function (){
+		var selector = "(node[amenity=bicycle_parking](%BBOX%);node[amenity=bicycle_repair_station](%BBOX%);node[shop=bicycle](%BBOX%););out;"+
+					"(way[amenity=bicycle_parking](%BBOX%);way[amenity=bicycle_repair_station](%BBOX%);way[shop=bicycle](%BBOX%););out center;";
+
+		this._poifetcher = new L.OverpassFetcher({
+					minZoom:13,
+					selector: selector,
+					layer: this._poiLayer,
+					createMarker: 	function(e){
+						var pos = new L.LatLng(e.lat, e.lon);
+						marker = new L.MarkerPOI(pos, {element: e,riseOnHover: true});
+						if(e.tags["name"] != undefined)
+							marker.bindLabel(e.tags["name"]);
+						return marker;
+					},
+					overpassQueue: this._overpassQueue});
+	},
+
+	hideHazardLayer: function(){
+		this._hideLayer(this._hazardLayer);
+		this._hideLayer(this._hazardfetcher);
+	},
+
+	showHazardLayer: function(){
+		this._showLayer(this._hazardLayer);
+		this._showLayer(this._hazardfetcher);
+	},
+
+	_addHazardPart: function(){
+		var selector = "(node['hazard:bicycle'](%BBOX%););out;(way['hazard:bicycle'](%BBOX%););out center;";
+		this._hazardfetcher = new L.OverpassFetcher({
+					minZoom:13,
+					selector: selector,
+					layer: this._hazardLayer,
+					createMarker: 	function(e){
+						var pos = new L.LatLng(e.lat, e.lon);
+						marker=new L.MarkerPOI(pos, {element: e,riseOnHover: true});
+						if(e.tags["description"] != undefined)
+							marker.bindLabel(e.tags["description"]);
+						return marker;
+					},
+					overpassQueue: this._overpassQueue});
+	},
+
+	hideWayLayer: function(){
+		this._hideLayer(this._wayLayer);
+		this._hideLayer(this._wayfetcher);
+	},
+
+	showWayLayer: function(){
+		this._showLayer(this._wayLayer);
+		this._showLayer(this._wayfetcher);
+	},
+
+	_addWayPart: function(){
+		var selector =	//one way for cars no for bc
+				'(way["oneway:bicycle"="no"]["oneway"="yes"](%BBOX%);'+
+				'way["oneway:bicycle"="no"]["oneway"="-1"](%BBOX%);'+
+				//opposite lane
+				'way["cycleway:left"="opposite_lane"](%BBOX%);'+
+				'way["cycleway:right"="opposite_lane"](%BBOX%);'+
+				'way["cycleway"="opposite_lane"](%BBOX%);'+
+				//lane
+				'way["cycleway:left"="lane"](%BBOX%);'+
+				'way["cycleway:right"="lane"](%BBOX%);'+
+				'way["cycleway"="lane"](%BBOX%);'+
+				'way["cycleway:lanes"~"designated"](%BBOX%);'+
+				//cycleways
+				'way["highway"="cycleway"](%BBOX%);'+
+				'way["bicycle"="designated"](%BBOX%););out geom;';
+		var _this = this;
+	 	this._wayfetcher = new L.OverpassFetcher({
+			minZoom:13,
+			selector: selector,
+			createPolyline:function(ll,el){ return _this._createWay(ll,el);},
+			layer: this._wayLayer,
+			overpassQueue: this._overpassQueue});
+	},
+
+	hideTrailLayer: function(){
+		this._hideLayer(this._wayLayer);
+		this._hideLayer(this._wayfetcher);
+	},
+
+	showTrailLayer: function(){
+		this._showLayer(this._trailLayer);
+		this._showLayer(this._trailfetcher);
+	},
+
+	_addTrailPart: function(){
+		var selector = 'relation[route=bicycle](%BBOX%);out;'+
+					'way(r)(%BBOX%);out ids tags geom;';
+		var _this = this;
+		this._trailfetcher = new L.OverpassFetcher({
+			minZoom:13,
+			selector: selector,
+			createPolyline: function(nn,el){ return _this._createTrail(nn,el);},
+			layer: this._trailLayer,
+			overpassQueue: this._overpassQueue});
+	},
+
+	_checkTag: function (tags,key,value,type){
+		if(tags.hasOwnProperty(key) && (type == '=' || type == '~')){
+			if(tags[key] == value && type == '=')
+				return true;
+			if(tags[key].search(value) != -1)
+				return true;
+		}else if(!tags.hasOwnProperty(key) && type == '!='){
+			return true;
+		}else if(tags.hasOwnProperty(key) && tags[key] != value && type == '!='){
+			return true;
+		}
+		return false;
+	},
+
+	_createWay: function(ll,el){
+		var feature = L.polyline( ll);
+		var color = "blue";
+
+		if(this._checkTag(el.tags,"oneway:bicycle","no",'=') && this._checkTag(el.tags,"oneway","yes",'='))
+			color = 'yellow';
+		if(this._checkTag(el.tags,"oneway:bicycle","no",'=') && this._checkTag(el.tags,"oneway","-1",'='))
+			color = 'yellow';
+
+		if(this._checkTag(el.tags,"cycleway:left","lane",'=')){
+			color = 'green';
+
+		}
+		if(this._checkTag(el.tags,"cycleway:right","lane",'=')){
+			color = 'green';
+		}
+
+		if(this._checkTag(el.tags,"cycleway","lane",'='))
+			color = 'green';
+
+		if(this._checkTag(el.tags,"cycleway:left","opposite_lane",'=')){
+			color = 'orange';
+		}
+
+		if(this._checkTag(el.tags,"cycleway:right","opposite_lane",'=')){
+			color = 'orange';
+		}
+
+		if(this._checkTag(el.tags,"cycleway","opposite_lane",'='))
+			color = 'orange';
+
+		if(this._checkTag(el.tags,"highway","cycleway",'='))
+			color = 'red';
+		if(this._checkTag(el.tags,"bicycle","designated",'='))
+			color = 'red';
+
+		if(this._checkTag(el.tags,"foot","designated",'=') && this._checkTag(el.tags,"bicycle","designated",'=') && this._checkTag(el.tags,"segregated","yes",'!='))
+			color = 'purple';
+		if(this._checkTag(el.tags,"foot","designated",'=') && this._checkTag(el.tags,"highway","cycleway",'=') && this._checkTag(el.tags,"segregated","yes",'!='))
+			color = 'purple';
+
+		feature.setStyle({'color':color});
+		return feature;
+	},
+	_createTrail: function (ll,el){
+		var array = [];
+		var rlength = Object.size(el.relations);
+		var i = 0;
+		for(var rel in el.relations){
+			console.log(el.relations[rel]);
+			var n = L.polyline( ll);
+			var color = el.relations[rel].tags["colour"];
+			console.log(color);
+			n.setStyle({'color': color});
+		
+			n.setOffset((i-rlength+1)*6);
+			array.push(n);
+			++i;
+		}
+		return array;
+	}
+
+});
--- js/main.js
+++ js/main.js
@@ -72,137 +72,6 @@ function ScaleContentToDevice(){
 			$(".ui-content").height();
     $(".ui-content").height(content);
 }
-//====================================================
-function newHazard(e){
-	var pos = new L.LatLng(e.lat, e.lon);
-	marker=(new L.MarkerPOI(pos, {element: e,riseOnHover: true}));
-	if(e.tags["description"] != undefined)
-		marker.bindLabel(e.tags["description"]);
-	return marker;
-}
-
-function newPoint(e){
-	var pos = new L.LatLng(e.lat, e.lon);
-	marker=(new L.MarkerPOI(pos, {element: e,riseOnHover: true}));
-	if(e.tags["name"] != undefined)
-		marker.bindLabel(e.tags["name"]);
-	return marker;
-}
-
-function addEvents(id,layer,req_layer){
-	var status = 0;
-	$(id).on("s_change",function(){
-		console.log(id);
-		if($(this).hasClass("ui-btn-active") && status != 2){
-			status = 2;
-			if(!map.hasLayer()){
-				console.log("Add");
-				map.addLayer(layer);
-				map.addLayer(req_layer);
-			}
-		}else if(status != 1){
-			status = 1;
-			map.removeLayer(layer);
-			map.removeLayer(req_layer);
-		}
-	});
-}
-
-var overpassQueue = new L.OverpassQueue({
-			//onLoadingStarted: function(){$("#text-zbox-loading").css("display","inline");},
-			//onLoadingFinished: function(){$("#text-zbox-loading").css("display","none");}
-		});
-
-function createPOILayers(layer,selector,id,newPOI){
-	var overpass_f = new L.OverpassFetcher({minZoom:13,selector: selector, layer: layer,createMarker: newPOI,overpassQueue: overpassQueue});
-	addEvents(id,layer,overpass_f);
-}
-//====================================================
-function createWayLayers(layer,selector,id, createPolyline){
-	var overpass_f;
-	overpass_f = new L.OverpassFetcher({minZoom:13,selector: selector, createPolyline:createPolyline,layer: layer,overpassQueue: overpassQueue});
-	addEvents(id,layer,overpass_f);
-}
-
-function checkTag(tags,key,value,type){
-	if(tags.hasOwnProperty(key) && (type == '=' || type == '~')){
-		if(tags[key] == value && type == '=')
-			return true;
-		if(tags[key].search(value) != -1)
-			return true;
-	}else if(!tags.hasOwnProperty(key) && type == '!='){
-		return true;
-	}else if(tags.hasOwnProperty(key) && tags[key] != value && type == '!='){
-		return true;
-	}
-	return false;
-}
-var colors = {noneway:"yellow",opposite_lane:"orange",lane: "green", shared:"purple",cycleway:"red"};
-
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-};
-
-
-
-function createTrails(ll,el){
-	var array = [];
-	var rlength = Object.size(el.relations);
-	var i = 0;
-	for(var rel in el.relations){
-		console.log(el.relations[rel]);
-		var n = L.polyline( ll);
-		var color = el.relations[rel].tags["colour"];
-		console.log(color);
-		n.setStyle({'color': color});
-		
-		n.setOffset((i-rlength+1)*6);
-		array.push(n);
-		++i;
-	}
-	return array;
-}
-
-function createWays(ll,el){
-	var feature = L.polyline( ll);
-	var color = "blue";
-	if(checkTag(el.tags,"oneway:bicycle","no",'=') && checkTag(el.tags,"oneway","yes",'='))
-		color = 'yellow';
-	if(checkTag(el.tags,"oneway:bicycle","no",'=') && checkTag(el.tags,"oneway","-1",'='))
-		color = 'yellow';
-
-	if(checkTag(el.tags,"cycleway:left","lane",'='))
-		color = 'green';
-	if(checkTag(el.tags,"cycleway:right","lane",'='))
-		color = 'green';
-	if(checkTag(el.tags,"cycleway","lane",'='))
-		color = 'green';
-
-	if(checkTag(el.tags,"cycleway:left","opposite_lane",'='))
-		color = 'orange';
-	if(checkTag(el.tags,"cycleway:right","opposite_lane",'='))
-		color = 'orange';
-	if(checkTag(el.tags,"cycleway","opposite_lane",'='))
-		color = 'orange';
-
-	if(checkTag(el.tags,"highway","cycleway",'='))
-		color = 'red';
-	if(checkTag(el.tags,"bicycle","designated",'='))
-		color = 'red';
-
-	if(checkTag(el.tags,"foot","designated",'=') && checkTag(el.tags,"bicycle","designated",'=') && checkTag(el.tags,"segregated","yes",'!='))
-		color = 'purple';
-	if(checkTag(el.tags,"foot","designated",'=') && checkTag(el.tags,"highway","cycleway",'=') && checkTag(el.tags,"segregated","yes",'!='))
-		color = 'purple';
-
-
-	feature.setStyle({'color':color});
-	return feature;
-}
 
 function afterInit(){
 	i18n_gen();
@@ -233,7 +102,7 @@ function afterInit(){
 	map.addControl(zbox);
 	zbox.setText('<i id="#text-zbox-zoomin" data-i18n="Please, zoom in"></i><br/>'+
 		'<i id="#text-zbox-loading" data-i18n="Loading"></i>...');
-	map.on("zoomlevelschange",function(){
+	map.on("zoomend",function(){
 		if(map.getZoom() < 15)
 			$("#text-zbox-zoomin").css("display","inline");
 		else
@@ -248,19 +117,9 @@ function afterInit(){
 	adBox.setText(adString);
 	setInterval(function () {adBox.setText(adString);}, 60000*5);
 
-	//$("#text-zbox-loading").css("display","none");
+	var ownLayers = new L.OwnLayersPack({map:map});
 
-	//Create layers
-	createPOILayers(
-		L.markerClusterGroup({ disableClusteringAtZoom: 14,
-				iconCreateFunction: function(cluster) {
-					var childCount = cluster.getChildCount();
-					return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-poi', iconSize: new L.Point(40, 40) });
-				}}),
-		"(node[amenity=bicycle_parking](%BBOX%);node[amenity=bicycle_repair_station](%BBOX%);node[shop=bicycle](%BBOX%););out;"+
-		"(way[amenity=bicycle_parking](%BBOX%);way[amenity=bicycle_repair_station](%BBOX%);way[shop=bicycle](%BBOX%););out center;",
-		"#nav-pois",newPoint);
-
+	//POI
 	$("#nav-pois").on("mouseenter",function(){
 		box.setText('<div class="pois-sprite pois-sprite-shop-bicycle"></div><i id="#text-leg" class="legend-text" data-i18n="Shop"></i><br/>'+
 			'<div class="pois-sprite pois-sprite-amenity-bicycle_repair_station"></div><i id="#text-leg" class="legend-text" data-i18n="Repair station"></i><br/>'+
@@ -268,40 +127,22 @@ function afterInit(){
 			);
 		$(".legend-text").i18n();
 	});
+	$("#nav-pois").on("s_change",function(){
+		if($(this).hasClass("ui-btn-active"))ownLayers.showPOILayer();
+		else ownLayers.hidePOILayer();
+	});
 
-	createPOILayers(
-		L.markerClusterGroup({ disableClusteringAtZoom: 14,
-				iconCreateFunction: function(cluster) {
-					var childCount = cluster.getChildCount();
-					return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-hazard', iconSize: new L.Point(40, 40) });
-				}}),
-		"(node['hazard:bicycle'](%BBOX%););out;(way['hazard:bicycle'](%BBOX%););out center;",
-		"#nav-hazards",newHazard);
+	//HAZARD
 	$("#nav-hazards").on("mouseenter",function(){
 		box.setText('<div class="pois-sprite pois-sprite-hazard__bicycle"></div><i id="#text-leg" class="legend-text" data-i18n="Hazard"></i>');
 		$(".legend-text").i18n();
 	});
-	//Ways
-	var ways = L.featureGroup();
-	createWayLayers(ways,
-			//one way for cars no for bc
-			'(way["oneway:bicycle"="no"]["oneway"="yes"](%BBOX%);'+
-			'way["oneway:bicycle"="no"]["oneway"="-1"](%BBOX%);'+
-			//opposite lane
-			'way["cycleway:left"="opposite_lane"](%BBOX%);'+
-			'way["cycleway:right"="opposite_lane"](%BBOX%);'+
-			'way["cycleway"="opposite_lane"](%BBOX%);'+
-			//lane
-			'way["cycleway:left"="lane"](%BBOX%);'+
-			'way["cycleway:right"="lane"](%BBOX%);'+
-			'way["cycleway"="lane"](%BBOX%);'+
-			'way["cycleway:lanes"~"designated"](%BBOX%);'+
-			//cycleways
-			'way["highway"="cycleway"](%BBOX%);'+
-			'way["bicycle"="designated"](%BBOX%););out geom;'
-		,'#nav-cycleways',createWays);
+	$("#nav-pois").on("s_change",function(){
+		if($(this).hasClass("ui-btn-active"))ownLayers.showHazardLayer();
+		else ownLayers.hideHazardLayer();
+	});
 
-	//TODO: I can do it better.
+	//WAYS
 	$("#nav-cycleways").on("mouseenter",function(){
 		box.setText('<table class="nomargin"><tr><td><p style="color:'+colors.noneway+'">&#9646;</p></td><td><i id="#text-leg" class="legend-text" data-i18n="Not a one-way road for bicycles"></i></td></tr>'+
 			'<tr><td><p style="color:'+colors.opposite_lane+'">&#9646;</p></td><td><i id="#text-leg" class="legend-text" data-i18n="Opposite lane"></i></td></tr>'+
@@ -311,19 +152,20 @@ function afterInit(){
 			);
 		$(".legend-text").i18n();
 	});
+	$("#nav-cycleways").on("s_change",function(){
+		if($(this).hasClass("ui-btn-active"))ownLayers.showWayLayer();
+		else ownLayers.hideWayLayer();
+	});
 
-	//Trails
-	var trails = L.featureGroup();
-	createWayLayers(trails,
-			//one way for cars no for bc
-			'relation[route=bicycle](%BBOX%);out;'+
-			'way(r)(%BBOX%);out ids tags geom;'
-		,'#nav-trails',createTrails);
-
+	//TRAIL
 	$("#nav-trails").on("mouseenter",function(){
 		box.setText('<i id="#text-leg" class="legend-text" data-i18n="Colour on the map means trail colour"></i>'
 			);
 		$(".legend-text").i18n();
+	});
+	$("#nav-trails").on("s_change",function(){
+		if($(this).hasClass("ui-btn-active"))ownLayers.showTrailLayer();
+		else ownLayers.hideTrailLayer();
 	});
 
 
@@ -342,12 +184,16 @@ function afterInit(){
 			$(this).addClass("ui-btn-active");
 		}
 		$(this).trigger("s_change");
+		map.fire("moveend");
 	});
+
 	map.on("moveend", function(){
 		setCookie("lat", map.getCenter().lat,64);
 		setCookie("lon", map.getCenter().lng,64);
 		setCookie("zoom",map.getZoom(),     64);
 	} );
+
+	map.fire("moveend");
 	i18n.init();
 	$("[data-i18n]").i18n();	
 }
